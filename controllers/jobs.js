09788072a515b302f82b2974d6d4c4516a68fa55
billyh <billyhkaufman@outlook.com>
2020-08-13 13:33:17 -0400

    added basic error handling

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,39 +7,65 @@ module.exports = router;
 
 // INDEX
 // GET api/jobs
-router.get('/', (req, res) => {
-  // Use our Job model to find all of the documents
-  // in the jobs collection
-  // When found, they are passed down the promise chain
-  // to the `.then()` where we send the response as JSON
-  // with `res.json` and pass it any jobs found
-  Job.find().then((jobs) => res.json(jobs));
+router.get('/', (req, res, next) => {
+	// Use our Job model to find all of the documents
+	// in the jobs collection
+	// Then send all of the jobs back as json
+	Job.find()
+		.then((jobs) => res.json(jobs))
+		.catch(next);
 });
 
 // SHOW
 // GET api/jobs/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => {
-	Job.findById(req.params.id).then((job) => res.json(job));
+router.get('/:id', (req, res, next) => {
+	Job.findById(req.params.id)
+		.then((job) => {
+			if (!job) {
+				res.sendStatus(404);
+			} else {
+				res.json(job);
+			}
+		})
+		.catch(next);
 });
 
 // CREATE
 // POST api/jobs
-router.post('/', (req, res) => {
-	Job.create(req.body).then((job) => res.json(job));
+router.post('/', (req, res, next) => {
+	Job.create(req.body)
+		.then((job) => res.status(201).json(job))
+		.catch(next);
 });
 
 // UPDATE
 // PUT api/jobs/5a7db6c74d55bc51bdf39793
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
 	Job.findOneAndUpdate({ _id: req.params.id }, req.body, {
 		new: true,
-	}).then((job) => res.json(job));
+	})
+		.then((job) => {
+			if (!job) {
+				res.sendStatus(404);
+			} else {
+				res.json(job);
+			}
+		})
+		.catch(next);
 });
 
 // DESTROY
 // DELETE api/jobs/5a7db6c74d55bc51bdf39793
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
 	Job.findOneAndDelete({
 		_id: req.params.id,
-	}).then((job) => res.json(job));
+	})
+		.then((job) => {
+			if (!job) {
+				res.sendStatus(404);
+			} else {
+				res.sendStatus(204);
+			}
+		})
+		.catch(next);
 });
